92b98b9d383719a193ed4555fb550d1da3c84c41
Valters Meirēns <valters.meirens@tieto.com>
2018-12-19 10:16:12 +0200

    Added tty to gitsecret command

--- exec/gitsecret.bat
+++ exec/gitsecret.bat
@@ -2,5 +2,5 @@
 IF "%1"=="addperson" (
     cat %2 | docker run --rm -i -v %cd%:/usr/repo git-secret bash -c "gpg --import && git secret tell %3"
 ) ELSE (
-    docker run --rm -i -v %cd%:/usr/repo git-secret git secret %*
+    docker run --rm -it -v %cd%:/usr/repo git-secret git secret %*
 )
